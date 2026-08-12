2ff49c5285c9bb2efce0b7de76fae7dd2dcf8083
monu9772 <rm9326826@gmail.com>
2026-08-12 21:28:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const telegramButton = document.querySelector(".telegram-btn");
 
-  if (telegramButton) {
+  if (!telegramButton) return;
 
-    telegramButton.addEventListener("click", function () {
+  telegramButton.addEventListener("click", function () {
+    if (typeof fbq === "function") {
+      fbq("track", "Lead");
+    }
+  });
 
-      if (typeof fbq === "function") {
-        fbq("track", "Lead");
-      }
+  // 10 seconds ke baad Telegram open
+  setTimeout(function () {
 
-    });
+    if (typeof fbq === "function") {
+      fbq("track", "Lead");
+    }
 
-  }
+    window.location.href =
+      "https://t.me/+XilBMDMFRYcwNDQ1";
+
+  }, 10000);
 
 });
